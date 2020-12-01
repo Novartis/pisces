88d053bbef42fa3541f4255cad2009346ef52312
Matt Shirley <mdshw5@gmail.com>
2020-12-01 10:36:29 -0500

    Fixing test

--- pisces/tests/test_cli.py
+++ pisces/tests/test_cli.py
@@ -3,10 +3,8 @@ from pisces.cli import create_parser
 
 
 class TestRun(TestCase):
-    def setUp(self):
-        self.parser = create_parser()
 
     def test_defaults_config(self):
         "Test that default configuration is set."
-        args = self.parser.parse_args(['run', '-fq1', 'data/test1.fastq'])
+        args = create_parser().parse_args('submit -m metadata.csv')
         assert 'human' in args.conf.keys()
