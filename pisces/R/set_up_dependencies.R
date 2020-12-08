c2750593ceea9de00271b8afbb8d153c6e4b7418
Matt Shirley <mdshw5@gmail.com>
2020-12-08 09:31:51 -0500

    Rely on renv bioconductor bootstrapping

--- pisces/R/set_up_dependencies.R
+++ pisces/R/set_up_dependencies.R
@@ -10,13 +10,9 @@ getScriptPath <- function() {
 }
 script.dir <- getScriptPath()
 setwd(script.dir)
-if (!requireNamespace("renv", quietly = TRUE)) { 
-    install.packages("renv",  repos='http://cran.us.r-project.org') }
+install.packages("renv",  repos='http://cran.us.r-project.org') }
 renv::settings$use.cache(FALSE, persist = TRUE)
 renv::consent(provided = TRUE)
 renv::activate()
-if (!requireNamespace("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager",  repos='http://cran.us.r-project.org') }
-options(repos = BiocManager::repositories())
 renv::hydrate(packages=renv::dependencies()$Package)
 renv::snapshot()
